e97eb977c078af86e0a01b1957b4f0923f43ebe1
kavishgandhi <kavishgandhi@kavishs-mbp.wireless-1x.unc.edu>
2019-08-25 14:01:03 -0400

    New Code for A1Novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,53 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		int customers = scan.nextInt();
+		String first[]= new String[customers];
+		String last[]= new String[customers];
+		int items=0;
+		
+		
+		double price_total[] = new double[customers];
+		
+		
+		for(int i=0; i<customers; i++)
+		{
+			first[i] = scan.next();
+			last[i] = scan.next();
+			items = scan.nextInt();		
+			
+			int quantity[] = new int[items];
+			String item[] = new String[items];
+			double price[] = new double[items];
+			
+			for(int j=0; j< quantity.length; j++)
+			{
+				 quantity[j]=scan.nextInt();
+				 item[j] = scan.next();
+				 
+				 price[j] = scan.nextDouble();		
+				 
+			}			
+			price_total[i] = compute_total(quantity, price);
+			
+		}
+		
+		for(int i=0; i<customers; i++)
+		{
+			
+			System.out.println(first[i].charAt(0)+". "+last[i]+": "+price_total[i]);
+		}
+			
+	}
+
+	public static double compute_total(int quantity[], double price[])
+	{
+		double total=0.00;
+		for(int i=0; i<quantity.length; i++)
+		{
+			total=total+(quantity[i]*price[i]);
+		}
+		
+		return total;
 	}
 }
